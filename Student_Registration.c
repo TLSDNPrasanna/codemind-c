ccebaa9f532e47517a7aedea7ef04578e5569d19
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2023-12-09 08:00:41 +0530

    #include<stdio.h> int main() {     int T;     scanf("%d",&T);     int N,M,K;     scanf("%d%d%d",&N,&M,&K);     if(N<(M-K))     printf("YES ");     else     printf("NO ");
       int n,m,k;
       scanf("%d%d%d",&n,&m,&k);
       if(n<(m-k))
       printf("YES
    ");
       else
       printf("NO
    ");
    }

---
+++ Student_Registration.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    int N,M,K;
+    scanf("%d%d%d",&N,&M,&K);
+    if(N<(M-K))
+    printf("YES
+");
+    else
+    printf("NO
+");
+   
+   int n,m,k;
+   scanf("%d%d%d",&n,&m,&k);
+   if(n<(m-k))
+   printf("YES
+");
+   else
+   printf("NO
+");
+}
+   
+   
