f98edb447959a732e46833f19225591c95f137b3
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2023-12-19 10:57:40 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int c=0;     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         c=i;     }     printf("%d",c); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        c=i;
+    }
+    printf("%d",c);
+}
