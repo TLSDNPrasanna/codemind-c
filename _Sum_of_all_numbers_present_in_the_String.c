c90236c2c5d6dcffbb33760a0696d393173f5264
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2023-12-31 12:47:13 +0530

    #include<stdio.h> int main() {     char str[80];     scanf("%[^ ]",&str);     int i,sum=0;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='0' && str[i]<='9')         sum=(sum+str[i])-'0';     }     printf("%d",sum); }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char str[80];
+    scanf("%[^
+]",&str);
+    int i,sum=0;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        sum=(sum+str[i])-'0';
+    }
+    printf("%d",sum);
+}
